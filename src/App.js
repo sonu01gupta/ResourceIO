20feb6c5480b466ce03f521dd22d5ef16600f28b
sonu01gupta <107051713+sonu01gupta@users.noreply.github.com>
2024-02-14 18:29:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Header from './components/Header'
+import Home from './components/Home'
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Services from './components/Services';
+import LocomotiveScroll from 'locomotive-scroll';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import "./styles/App.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/footer.scss";
+import "./styles/contact.scss";
+import "./styles/services.scss";
+import "./styles/mediaquerry.scss";
+
+const App = () => {
+  const locomotiveScroll = new LocomotiveScroll();
+  return (
+    <Router>
+
+    <Header/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/Contact" element={<Contact/>}/>
+            <Route path="/Services" element={<Services/>}/>
+
+            
+            <Route/>
+            <Route/>
+            <Route/>
+        </Routes>
+    <Footer/>
+    
+    
+    
+    
+    </Router>
+  )
+}
+
+export default App
